a3b791891552cb7b9d1ee8d1bd98fb23ad01480d
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-19 21:55:20 +0300

    Update dist

--- dist/config/index.js
+++ dist/config/index.js
@@ -13,15 +13,9 @@ var _cors = require('./cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
-var _env = require('env2');
-
-var _env2 = _interopRequireDefault(_env);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function configure(app) {
-    (0, _env2.default)('config.env'); // Load all environment variables
-
     (0, _express2.default)(app);
     (0, _cors2.default)(app);
 }
