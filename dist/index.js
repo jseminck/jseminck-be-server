70532d7d10e9b633e7d032c5a4a84ba8e2aa4ea4
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-19 17:00:52 +0300

    Make the exported function async and make it wait for configureServer, so we can initialize data

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.default = configureApp;
 
 require('babel-polyfill');
 
@@ -21,28 +20,56 @@ var _routes2 = _interopRequireDefault(_routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
 /**
  *
  * @param configureServer {Function}
  * @param configureRoutes {Function}
  */
-function configureApp(cfg) {
-    var app = (0, _express2.default)();
 
-    // Configure local server configuration and routes
-    (0, _config2.default)(app);
-    (0, _routes2.default)(app);
+exports.default = function () {
+    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(cfg) {
+        var app, port;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        app = (0, _express2.default)();
 
-    // Configure custom server configuration and routes
-    cfg.configureServer(app);
-    cfg.configureRoutes(app);
+                        // Configure local server configuration and routes
 
-    // set the port of our application
-    // process.env.PORT lets the port be set by Heroku
-    var port = process.env.PORT || 8080;
+                        _context.next = 3;
+                        return (0, _config2.default)(app);
 
-    app.listen(port);
-    console.log('Server running at http://127.0.0.1:' + port); // eslint-disable-line no-console
+                    case 3:
+                        (0, _routes2.default)(app);
 
-    return app;
-}
+                        // Configure custom server configuration and routes
+                        cfg.configureServer(app);
+                        cfg.configureRoutes(app);
+
+                        // set the port of our application
+                        // process.env.PORT lets the port be set by Heroku
+                        port = process.env.PORT || 8080;
+
+
+                        app.listen(port);
+                        console.log('Server running at http://127.0.0.1:' + port); // eslint-disable-line no-console
+
+                        return _context.abrupt('return', app);
+
+                    case 10:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, this);
+    }));
+
+    function configureApp(_x) {
+        return ref.apply(this, arguments);
+    }
+
+    return configureApp;
+}();
