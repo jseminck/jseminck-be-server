e30d23cf3ed3fcc60df24330b4e96f54b0d5fe47
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-07-16 21:07:49 +0300

    Build

--- dist/routes/ping.js
+++ dist/routes/ping.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
@@ -10,12 +10,12 @@ var get = function () {
             while (1) {
                 switch (_context.prev = _context.next) {
                     case 0:
-                        return _context.abrupt("return", res.status(200).send({
-                            ping: "Hello World"
+                        return _context.abrupt('return', res.status(200).send({
+                            time: _moment2.default.unix()
                         }));
 
                     case 1:
-                    case "end":
+                    case 'end':
                         return _context.stop();
                 }
             }
@@ -28,6 +28,12 @@ var get = function () {
 }();
 
 exports.default = configureAuthenticationRoutes;
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
 
