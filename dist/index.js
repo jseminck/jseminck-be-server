c298fd6c4287d5e32b7dc1ca6a225ed180ce6437
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-07-17 18:43:04 +0300

    Add jade index view

--- dist/index.js
+++ dist/index.js
@@ -21,11 +21,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 /**
  *
- * @param configureServer {Function}
- * @param configureRoutes {Function}
+ * @param {Object} cfg
+ *   @param {Function} cfg.configureServer
+ *   @param {Function} cfg configureRoutes
+ *   @param {Jade} cfg.index
  */
 function configureApp(cfg) {
     var app = (0, _express2.default)();
+
+    app.set('view engine', 'jade');
+    app.get('/', function (req, res) {
+        return res.render(cfg.index);
+    });
 
     // Configure local server configuration and routes
     (0, _config2.default)(app);
