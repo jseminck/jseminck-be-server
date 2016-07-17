ae83078317611e66b1d3c7a6a1a5a9c726621e7e
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-07-17 14:09:49 +0300

    Allow CORS from anywhere

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = configureCors;
 function configureCors(app) {
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
