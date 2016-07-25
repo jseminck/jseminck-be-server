4ce52c07c2ff8a7f8ce7845e3b55c9d3ff27a515
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-07-25 22:26:16 +0300

    Switch order of execution of local/cfg configure server, and also remove the static setup

--- dist/index.js
+++ dist/index.js
@@ -20,27 +20,26 @@ var _routes2 = _interopRequireDefault(_routes);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
- *
  * @param {Object} cfg
  *   @param {Function} cfg.configureServer
  *   @param {Function} cfg configureRoutes
- *   @param {Jade} cfg.index
+ *   @param {String} cfg.staticFolder
+ *   @param {String} cfg.index HTML to serve for the index file
  */
 function configureApp(cfg) {
     var app = (0, _express2.default)();
 
-    // console.log("cfg.index", cfg.index);
     app.get('/', function (req, res) {
         return res.send(cfg.index);
     });
 
-    // Configure local server configuration and routes
-    (0, _config2.default)(app);
-    (0, _routes2.default)(app);
-
     // Configure custom server configuration and routes
     cfg.configureServer(app);
     cfg.configureRoutes(app);
+
+    // Configure local server configuration and routes
+    (0, _config2.default)(app);
+    (0, _routes2.default)(app);
 
     // set the port of our application
     // process.env.PORT lets the port be set by Heroku
